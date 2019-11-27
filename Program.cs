3ee3f28764d63e91042ec03c2fc36aa27f03e2a7
Meghana R <meghana.r@sonata-software.com>
2019-11-27 15:16:31 +0530

    Changes the param to 100

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+delegate int NumberChanger(int n);
+
 namespace Training
 {
     class Program
     {
+
+        static int num = 10;
+        
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create delegate instances //
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            //calling the methods using the delegate objects
+            nc1(1000);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(200);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
         }
     }
 }
